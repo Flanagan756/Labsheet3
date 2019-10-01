ccfcf55bb0cd2c37d2c41416d378dfce86c3f338
S00189591 <S00189591@LAB-B1041-28>
2019-10-01 12:59:39 +0100

    In Lab Questions

---
+++ LabSheet 3/Q1/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Car
+    {
+        //Properties
+        private string make;
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        private int currentSpeed;
+        public int CurrentSpeed
+        {
+            get { return this.currentSpeed; }
+            set { this.currentSpeed = value; }
+        }
+
+        private double engineSize;
+        public double EngineSize
+        {
+            get { return this.engineSize; }
+            set { this.engineSize = value; }
+        }
+
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine("Car Make: {0}", Make);
+            Console.WriteLine("Car Model: {0}",Model);
+            Console.WriteLine("Car Current Speed: {0}",CurrentSpeed);
+            Console.WriteLine("Car Engine Size: {0}",EngineSize);
+        }
+
+        public override string ToString() //How it will be written to the main
+        {
+            return string.Format("Car Make: {0}" + "Car Model: {1}" + "Car Current Speed: {2} Car Engine Size: {3}", Make, Model, CurrentSpeed, EngineSize);
+        }
+    }
+
+}
+   
---
+++ LabSheet 3/Q1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //gives attributes values
+            Car car1 = new Car();
+            car1.Make = "Ford";
+            car1.Model = "Focus";
+            car1.CurrentSpeed = 19;
+            car1.EngineSize = 1.6;
+
+            Car car2 = new Car();
+            car2.Make = "Skoda";
+            car2.Model = "Horizon";
+            car2.CurrentSpeed = 22;
+            car2.EngineSize = 0.7;
+
+            //Displays Car info using method from car class
+            Console.WriteLine("Car 1");
+            car1.DisplayCarInfo();
+
+            Console.WriteLine("Car 2");
+            car2.DisplayCarInfo();
+
+
+        }
+       
+      
+    }
+}
